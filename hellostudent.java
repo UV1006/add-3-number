5ff77098324042ce0ff009716adec844e0882bff
UV1006 <udayvyas743@gmail.com>
2025-05-07 13:12:31 -0700

    this is java commit 5

--- hellostudent.java
+++ hellostudent.java
@@ -3,5 +3,6 @@ public class hellostudent {
     public static void main(String[] args) {
         String studentName = "Uday,Bhupendra"; 
         System.out.println("Hello, " + studentName + "!");
+	System.out.println("Welcome to C-DAC");
     }
 }
