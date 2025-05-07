0706835fecaebb1c300ae3b039e87468ef79a872
UV1006 <udayvyas743@gmail.com>
2025-05-07 12:40:51 -0700

    this is java commit

---
+++ hellostudent.java
@@ -0,0 +1,7 @@
+public class HelloStudent {
+
+    public static void main(String[] args) {
+        String studentName = "Uday,Bhupendra"; 
+        System.out.println("Hello, " + studentName + "!");
+    }
+
